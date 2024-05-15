42ec31754be6dfb530c985ff146a2bafbbdd1e65
Jeremy Stretch <jstretch@netboxlabs.com>
2024-05-15 10:26:15 -0400

    Introduce DynamicSchemaDict

--- netbox_vcs/utilities.py
+++ netbox_vcs/utilities.py
@@ -1,8 +1,32 @@
 from netbox.context import current_request
 
 __all__ = (
+    'DynamicSchemaDict',
     'get_active_context',
 )
+
+
+class DynamicSchemaDict(dict):
+    """
+    Behaves like a normal dictionary, except for keys beginning with "schema_". Any lookup for
+    "schema_*" will return the default configuration extended to include the search_path option.
+    """
+    def __getitem__(self, item):
+        if type(item) is str and item.startswith('schema_'):
+            if schema := item.removeprefix('schema_'):
+                default_config = super().__getitem__('default')
+                return {
+                    **default_config,
+                    'OPTIONS': {
+                        'options': f'-c search_path={schema},public'
+                    }
+                }
+        return super().__getitem__(item)
+
+    def __contains__(self, item):
+        if type(item) is str and item.startswith('schema_'):
+            return True
+        return super().__contains__(item)
 
 
 def get_active_context():
