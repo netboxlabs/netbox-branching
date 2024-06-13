b387111600f21245c914f5928bd8528929bd2a49
Jeremy Stretch <jstretch@netboxlabs.com>
2024-06-13 16:03:04 -0400

    Fix request tests

--- netbox_vcs/tests/test_request.py
+++ netbox_vcs/tests/test_request.py
@@ -2,6 +2,7 @@ from django.contrib.auth import get_user_model
 from django.test import Client, TestCase
 from django.urls import reverse
 
+from netbox_vcs.choices import ContextStatusChoices
 from netbox_vcs.contextvars import active_context
 from netbox_vcs.models import Context
 
@@ -14,6 +15,7 @@ class RequestTestCase(TestCase):
 
         context = Context(name='Context1')
         context.save()
+        context.provision()
 
     def setUp(self):
         # Initialize the test client
@@ -27,13 +29,11 @@ class RequestTestCase(TestCase):
         url = reverse('home')
         response = self.client.get(f'{url}?_context={context.schema_id}')
         self.assertEqual(response.status_code, 200)
-        self.assertIsNotNone(active_context.get(), msg="Context not set by middleware")
-        self.assertEqual(active_context.get(), context, msg="Incorrect context set by middleware")
-
-        # Verify that the context remains active for successive requests
-        response = self.client.get(url)
-        self.assertIsNotNone(active_context.get(), msg="Context not retained on successive request")
-        self.assertEqual(active_context.get(), context, msg="Incorrect context set by middleware")
+        self.assertEqual(
+            self.client.cookies['active_context'].value,
+            context.schema_id,
+            msg="Cookie was not set on response"
+        )
 
     def test_deactivate_context(self):
         # Attach active_context cookie to the test client
@@ -46,4 +46,4 @@ class RequestTestCase(TestCase):
         url = reverse('home')
         response = self.client.get(f'{url}?_context=')
         self.assertEqual(response.status_code, 200)
-        self.assertIsNone(active_context.get(), msg="Context still active after being deactivated")
+        self.assertEqual(self.client.cookies['active_context'].value, '', msg="Cookie was not deleted")
