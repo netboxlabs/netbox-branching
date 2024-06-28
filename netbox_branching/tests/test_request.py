a3dc763a73b377694b12ec084f3225f049069347
Jeremy Stretch <jstretch@netboxlabs.com>
2024-06-28 09:50:44 -0400

    Partially fix RequestTestCase

--- netbox_branching/tests/test_request.py
+++ netbox_branching/tests/test_request.py
@@ -1,25 +1,24 @@
 from django.contrib.auth import get_user_model
-from django.test import Client, TestCase
+from django.test import Client, TransactionTestCase
 from django.urls import reverse
 
+from netbox_branching.choices import BranchStatusChoices
 from netbox_branching.constants import COOKIE_NAME, QUERY_PARAM
 from netbox_branching.models import Branch
 
 
-class RequestTestCase(TestCase):
-
-    @classmethod
-    def setUpTestData(cls):
-        get_user_model().objects.create_user(username='testuser')
-
-        branch = Branch(name='Branch 1')
-        branch.save(provision=False)
-        branch.provision()
+class RequestTestCase(TransactionTestCase):
 
     def setUp(self):
         # Initialize the test client
+        user = get_user_model().objects.create_user(username='testuser')
         self.client = Client()
-        self.client.force_login(get_user_model().objects.first())
+        self.client.force_login(user)
+
+        # Create a Branch
+        branch = Branch(name='Branch 1')
+        branch.status = BranchStatusChoices.READY  # Fake provisioning
+        branch.save(provision=False)
 
     def test_activate_branch(self):
         branch = Branch.objects.first()
@@ -28,10 +27,11 @@ class RequestTestCase(TestCase):
         url = reverse('home')
         response = self.client.get(f'{url}?{QUERY_PARAM}={branch.schema_id}')
         self.assertEqual(response.status_code, 200)
+        self.assertIn(COOKIE_NAME, self.client.cookies, msg="Cookie was not set on response")
         self.assertEqual(
             self.client.cookies[COOKIE_NAME].value,
             branch.schema_id,
-            msg="Cookie was not set on response"
+            msg="Branch ID set in cookie is incorrect"
         )
 
     def test_deactivate_branch(self):
