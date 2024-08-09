982141545eb8b04963d63d862ed7713d1bd4ac04
Michal Fiedorowicz <mfiedorowicz@netboxlabs.com>
2024-08-09 12:38:42 +0200

    add docker-compose setup
    based on NetBox v4.1-beta1-2.9.1 image with netbox_branching plugin installed
    
    Signed-off-by: Michal Fiedorowicz <mfiedorowicz@netboxlabs.com>

---
+++ docker/configuration/plugins.py
@@ -0,0 +1,7 @@
+# Add your plugins and plugin settings here.
+# Of course uncomment this file out.
+
+# To learn how to build images with your required plugins
+# See https://github.com/netbox-community/netbox-docker/wiki/Using-Netbox-Plugins
+
+PLUGINS = ["netbox_branching"]
