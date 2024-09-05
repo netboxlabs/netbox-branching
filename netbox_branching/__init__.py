c1b44e9e894d6d18d156f7b697bb94444634680c
Jeremy Stretch <jstretch@netboxlabs.com>
2024-09-05 10:48:37 -0400

    Release v0.4.0

--- netbox_branching/__init__.py
+++ netbox_branching/__init__.py
@@ -9,7 +9,7 @@ class AppConfig(PluginConfig):
     name = 'netbox_branching'
     verbose_name = 'NetBox Branching'
     description = 'A git-like branching implementation for NetBox'
-    version = '0.3.1'
+    version = '0.4.0'
     base_url = 'branching'
     min_version = '4.1'
     middleware = [
