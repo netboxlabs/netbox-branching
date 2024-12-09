b7697d911fade2c2c215d272655d3f801cae964d
Arthur Hanson <worldnomad@gmail.com>
2024-12-09 09:58:21 -0800

    148 cleanup

--- netbox_branching/script_context_manager.py
+++ netbox_branching/script_context_manager.py
@@ -3,8 +3,7 @@ from .utilities import activate_branch, get_active_branch
 
 
 def NetBoxScriptContextManager(request):
-    branch = get_active_branch(request)
-    if branch:
+    if branch := get_active_branch(request)
         return activate_branch(branch)
     else:
         return nullcontext()
