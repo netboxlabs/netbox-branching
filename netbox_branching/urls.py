e749536292ca1d85e6cadc211faf9758dfb94dda
Brian Tiemann <btman@mac.com>
2025-02-04 10:35:14 -0500

    Fix paths

--- netbox_branching/urls.py
+++ netbox_branching/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     # Branches
     path('branches/', views.BranchListView.as_view(), name='branch_list'),
     path('branches/add/', views.BranchEditView.as_view(), name='branch_add'),
-    path('branches/import/', views.BranchBulkImportView.as_view(), name='branch_bulk_import'),
-    path('branches/edit/', views.BranchBulkEditView.as_view(), name='branch_edit'),
+    path('branches/import/', views.BranchBulkImportView.as_view(), name='branch_import'),
+    path('branches/edit/', views.BranchBulkEditView.as_view(), name='branch_bulk_edit'),
     path('branches/delete/', views.BranchBulkDeleteView.as_view(), name='branch_bulk_delete'),
     path('branches/<int:pk>/', include(get_model_urls('netbox_branching', 'branch'))),
 
