8a453fd820ad7cfb5cdc088e8ec82aee0d069922
Jason Novinger <jnovinger@gmail.com>
2026-01-20 13:08:12 -0600

    Release v0.8.1

--- netbox_branching/__init__.py
+++ netbox_branching/__init__.py
@@ -12,7 +12,7 @@ class AppConfig(PluginConfig):
     name = 'netbox_branching'
     verbose_name = 'NetBox Branching'
     description = 'A git-like branching implementation for NetBox'
-    version = '0.8.0'
+    version = '0.8.1'
     base_url = 'branching'
     # Remember to update COMPATIBILITY.md when modifying the minimum/maximum supported NetBox versions.
     min_version = '4.4.1'
