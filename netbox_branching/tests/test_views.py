d3c7161f691af64b8ae377434eb840c719a6f463
Arthur <worldnomad@gmail.com>
2026-01-20 16:49:06 -0800

    add test

--- netbox_branching/tests/test_views.py
+++ netbox_branching/tests/test_views.py
@@ -1,6 +1,14 @@
+from django.contrib.messages import get_messages
+from django.db import connections
+from django.test import TransactionTestCase, override_settings
+from django.urls import reverse
 from django_rq import get_queue
 
+from dcim.models import Site
+from netbox_branching.choices import BranchStatusChoices
+from netbox_branching.constants import QUERY_PARAM
 from netbox_branching.models import Branch
+from netbox_branching.utilities import activate_branch
 from utilities.testing import ViewTestCases, create_tags
 
 
@@ -50,3 +58,53 @@ class BranchTestCase(ViewTestCases.PrimaryObjectViewTestCase):
     def tearDown(self):
         # Clear jobs queue
         get_queue('default').connection.flushall()
+
+
+class BranchMiddlewareTestCase(TransactionTestCase):
+    serialized_rollback = True
+
+    def tearDown(self):
+        # Manually tear down any dynamic connections created for branches
+        for branch in Branch.objects.all():
+            if branch.connection_name in connections:
+                connections[branch.connection_name].close()
+
+    @override_settings(LOGIN_REQUIRED=False)
+    def test_redirect_on_404_during_branch_deactivation(self):
+        """
+        Test that deactivating a branch while viewing an object that only exists
+        in that branch redirects to the dashboard with a warning message.
+        """
+        # Create and provision a branch
+        branch = Branch(name='Test Branch')
+        branch.status = BranchStatusChoices.READY
+        branch.save(provision=False)
+        branch.provision(user=None)
+
+        # Create a site in the branch
+        with activate_branch(branch):
+            site = Site.objects.create(name='Branch Site', slug='branch-site')
+            site_pk = site.pk
+
+        # Get the URL for the site detail page
+        site_url = reverse('dcim:site', kwargs={'pk': site_pk})
+
+        # First, verify the site is accessible when the branch is active
+        response = self.client.get(f'{site_url}?{QUERY_PARAM}={branch.schema_id}')
+        self.assertEqual(response.status_code, 200)
+
+        # Now deactivate the branch while viewing the site (which only exists in the branch)
+        response = self.client.get(f'{site_url}?{QUERY_PARAM}=', follow=False)
+
+        # Should redirect to the dashboard
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, '/')
+
+        # Follow the redirect and check for the warning message
+        response = self.client.get(f'{site_url}?{QUERY_PARAM}=', follow=True)
+        messages = list(get_messages(response.wsgi_request))
+        self.assertEqual(len(messages), 1)
+        self.assertEqual(str(messages[0]), "The requested object does not exist in the current branch.")
+
+        # Clean up
+        branch.deprovision()
